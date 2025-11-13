a32568544273e914eb3cd0f0d5168216ed4a0387
Ferencz Roude <ferencz.roudet@gmail.com>
2025-11-13 20:09:42 +0100

    j'ai restructuré l'uml et réécris des trucs dans Object et grille

--- bataille_navale_java/src/batailleNavale/models/Object.java
+++ bataille_navale_java/src/batailleNavale/models/Object.java
@@ -3,9 +3,11 @@ package batailleNavale.models;
 public abstract class Object {
     private int x;
     private int y;
-
-    public Object(int x, int y){
+    private int size;
+    private int[][] squaresOccupied;
+    public Object(int x, int y, int size){
         this.x = x;
         this.y = y;
+        this.size = size;
     }
 }
--- bataille_navale_java/src/batailleNavale/models/grille/Grille.java
+++ bataille_navale_java/src/batailleNavale/models/grille/Grille.java
@@ -1,17 +1,22 @@
 package batailleNavale.models.grille;
 
+import batailleNavale.models.Object;
+
 public class Grille {
     private int height;
     private int width;
+    private Object[][] grille;
 
     public Grille(){
         this.height = 10;
         this.width = 10;
+        grille = new Object[10][10];
     }
 
     public Grille(int heigth, int width){
         this.height = heigth;
         this.width = width;
+        grille = new Object[10][10];
     }
 
 }
