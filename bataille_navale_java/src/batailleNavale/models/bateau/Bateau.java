319a8a1d079c548c2816227af01dd1d760eaa085
Ferencz Roude <ferencz.roudet@gmail.com>
2025-11-15 12:26:58 +0100

    quelques modifs

--- bataille_navale_java/src/batailleNavale/models/bateau/Bateau.java
+++ bataille_navale_java/src/batailleNavale/models/bateau/Bateau.java
@@ -8,10 +8,12 @@ public class Bateau extends Object {
     private int size;
     private int hits;
     private Types type;
+    private boolean orientation; //TRUE = VERTICAL ; FALSE = HORIZONTAL
 
-    public Bateau( int size, Types type){
+    public Bateau( int size, Types type, boolean orientation){
         super(size);
         this.type = type;
+        this.orientation = orientation;
     }
 
     public boolean isSunk(){
