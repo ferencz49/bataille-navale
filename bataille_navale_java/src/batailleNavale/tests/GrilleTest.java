0e9590156d319db340fb39d578e63e2cb6300ab7
[Ferencz] <[froudet]unistra.fr>
2025-11-18 20:24:19 +0100

    j'ai créé la fonction permettant de placer des objets sur la map avec les verifs nécessaires

--- bataille_navale_java/src/batailleNavale/tests/GrilleTest.java
+++ bataille_navale_java/src/batailleNavale/tests/GrilleTest.java
@@ -18,14 +18,17 @@ class GrilleTest {
         Bateau b = new Bateau(5, Types.Croiseur, true);
         Grille g = new Grille();
 
-        boolean result = g.setObject(5, 6, b);
+        boolean result = g.setObject(5, 6,true,1, b);
 
         assertTrue(result);           // Vérifie que l'ajout réussit
 
-        boolean result2 = g.setObject(11,15,b);
+        boolean result2 = g.setObject(11,15,true,1,b);
         assertFalse(result2);         // vérifie que l'ajout rate
 
-        boolean result3  = g.setObject(-1,15,b);
+        boolean result3  = g.setObject(-1,15,true, 1,b);
         assertFalse(result3);         // vérifie que l'ajout rate aussi
+
+        boolean result4 = g.setObject(3,3, true,3,b);
+        assertTrue(result4);
     }
 }
