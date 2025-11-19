6fcfa4bc5d4440417921d0d216e54814a2e0ed44
[Ferencz] <[froudet]unistra.fr>
2025-11-19 14:48:35 +0100

    on ne nait pas développeur, on le devient

--- bataille_navale_java/src/Main.java
+++ bataille_navale_java/src/Main.java
@@ -2,10 +2,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 
+import bataille_navale.models.bateau.Bateau;
+import bataille_navale.models.bateau.Types;
+import bataille_navale.models.grille.Grille;
 import bataille_navale.views.GameSettings;
 
 public class Main {
     public static void main(String[] args) {
-        GameSettings a = new GameSettings();
+        //GameSettings a = new GameSettings();
+
+        Grille g = new Grille();
+
+        Bateau b = new Bateau(4, Types.Croiseur, true);
+
+        g.setObject(3, 4, true, 3, b);
+
+        for (int i = 0; i < g.getGrille()[0].length; i++) {
+            for (int j = 0; j < g.getGrille()[1].length; j++) {
+                if (g.getGrille()[i][j] != null) {
+                    System.out.print("x\t");
+                } else {
+                    System.out.print(g.getGrille()[i][j] + "  ");
+                }
+            }
+            System.out.println();
+        }
+
+
     }
 }
---
+++ bataille_navale_java/src/bataille_navale/tests/GrilleTest.java
@@ -0,0 +1,34 @@
+package bataille_navale.tests;
+
+import bataille_navale.models.bateau.Bateau;
+import bataille_navale.models.bateau.Types;
+import bataille_navale.models.grille.Grille;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GrilleTest {
+
+    @Test
+    void getGrille() {
+    }
+
+    @Test
+    void setObject() {
+        Bateau b = new Bateau(5, Types.Croiseur, true);
+        Grille g = new Grille();
+
+        boolean result = g.setObject(5, 6,true,1, b);
+
+        assertTrue(result);           // Vérifie que l'ajout réussit
+
+        boolean result2 = g.setObject(11,15,true,1,b);
+        assertFalse(result2);         // vérifie que l'ajout rate
+
+        boolean result3  = g.setObject(-1,15,true, 1,b);
+        assertFalse(result3);         // vérifie que l'ajout rate aussi
+
+        boolean result4 = g.setObject(3,3, true,3,b);
+        assertTrue(result4);          // vérifie que l'ajoute réussit
+    }
+}
