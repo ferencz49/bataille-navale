cad5b023068754a36ce0017e339f8491d3e826b3
Maxime MALYS <malys@unistra.fr>
2025-11-28 11:06:24 +0100

    Ajout methodes create Boat

--- bataille_navale_java/src/bataille_navale/models/boat/Boat.java
+++ bataille_navale_java/src/bataille_navale/models/boat/Boat.java
@@ -5,12 +5,35 @@ import bataille_navale.Object;
 public class Boat extends Object {
     private int hits;
     private Types type;
-    private BoatDirection orientation; //TRUE = VERTICAL ; FALSE = HORIZONTAL
 
-    public Boat(int size, Types type, BoatDirection direction){
+    public Boat(int size, Types type){
         super(size);
         this.type = type;
-        this.orientation = direction;
+    }
+
+    public static Boat createPorteAvion(){
+        return new Boat(5, Types.PorteAvion);
+    }
+
+    public static Boat createCroiseur(){
+        return new Boat(4, Types.Croiseur);
+    }
+
+    public static Boat createContreTorpilleur(){
+        return new Boat(3, Types.ContreTorpilleur);
+    }
+
+    public static Boat createSousMarin(){
+        return new Boat(3, Types.SousMarin);
+    }
+
+    public static Boat createTorpilleur(){
+        return new Boat(2, Types.Torpilleur);
+    }
+
+    public int getSize()
+    {
+        return this.getSize();
     }
 
     public boolean isSunk(){
