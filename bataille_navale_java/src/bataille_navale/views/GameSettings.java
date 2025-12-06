38e19f9c753ebab2d58f63473b55ab6ff8a4b9c6
[Ferencz] <[froudet]unistra.fr>
2025-12-06 18:37:15 +0100

    ecran de config terminé

--- bataille_navale_java/src/bataille_navale/views/GameSettings.java
+++ bataille_navale_java/src/bataille_navale/views/GameSettings.java
@@ -13,7 +13,14 @@ public class GameSettings extends JFrame {
         JCheckBox ckb_mode_ile = new JCheckBox("mode ile");
         JLabel lbl_nb_porte_avions = new JLabel("Nombre de porte avions:");
         JSpinner spn_nb_porte_avions = new JSpinner();
-        JLabel lbl_nb_croiseurs = new JLabel("Nombre de porte avions:");
+        JLabel lbl_nb_croiseurs = new JLabel("Nombre de croiseurs:");
+        JSpinner spn_nb_croiseurs = new JSpinner();
+        JLabel lbl_nb_contre_torpilleurs = new JLabel("Nombre de contre torpilleurs:");
+        JSpinner spn_nb_contre_torpilleurs = new JSpinner();
+        JLabel lbl_nb_sous_marins = new JLabel("Nombre de sous-marins:");
+        JSpinner spn_nb_sous_marins = new JSpinner();
+        JLabel lbl_nb_torpilleurs = new JLabel("Nombre de torpilleurs:");
+        JSpinner spn_nb_torpilleurs = new JSpinner();
 
         JPanel layout_principal = new JPanel(new BorderLayout());
         layout_principal.add(btn_fin_config, BorderLayout.SOUTH);
@@ -21,11 +28,18 @@ public class GameSettings extends JFrame {
         JPanel layout_secondaire_centre = new JPanel();
         layout_secondaire_centre.setLayout(new BoxLayout(layout_secondaire_centre, BoxLayout.Y_AXIS));
         layout_secondaire_centre.add(Box.createVerticalStrut(20));
-        layout_secondaire_centre.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+        layout_secondaire_centre.setBorder(BorderFactory.createEmptyBorder(100, 300, 100, 300));
         layout_secondaire_centre.add(ckb_mode_ile);
         layout_secondaire_centre.add(lbl_nb_porte_avions);
         layout_secondaire_centre.add(spn_nb_porte_avions);
         layout_secondaire_centre.add(lbl_nb_croiseurs);
+        layout_secondaire_centre.add(spn_nb_croiseurs);
+        layout_secondaire_centre.add(lbl_nb_contre_torpilleurs);
+        layout_secondaire_centre.add(spn_nb_contre_torpilleurs);
+        layout_secondaire_centre.add(lbl_nb_sous_marins);
+        layout_secondaire_centre.add(spn_nb_sous_marins);
+        layout_secondaire_centre.add(lbl_nb_torpilleurs);
+        layout_secondaire_centre.add(spn_nb_torpilleurs);
         //on ajoute le layout qui contient les boutons permettant de setup la game au centre du layout principal
         layout_principal.add(layout_secondaire_centre, BorderLayout.CENTER);
 
