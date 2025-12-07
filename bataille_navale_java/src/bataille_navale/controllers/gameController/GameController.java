934434728bbfdcdfc8e0c5abacf5a7f851234532
Ferencz Roude <ferencz.roudet@gmail.com>
2025-12-07 12:12:02 +0100

    je fais une pause

--- bataille_navale_java/src/bataille_navale/controllers/gameController/GameController.java
+++ bataille_navale_java/src/bataille_navale/controllers/gameController/GameController.java
@@ -1,14 +1,24 @@
 package bataille_navale.controllers.gameController;
 
+import bataille_navale.views.AfficherGrille;
+
 public class GameController {
+    private AfficherGrille view;
     private ActionType selectedAction;
 
+    public GameController(AfficherGrille view){
+        this.view = view;
+
+        view.getUtiliserAttaque().addActionListener(e -> selectedAction = ActionType.BASIC_ATTACK);
+        view.getUtiliserBombe().addActionListener(e -> selectedAction = ActionType.BOMB);
+
+
+    }
+
     public void onBasicAttackClicked(){
-        this.selectedAction = ActionType.BASIC_ATTACK;
     }
 
     public void onBombAttackClicked(){
-        this.selectedAction = ActionType.BOMB;
     }
 
 
